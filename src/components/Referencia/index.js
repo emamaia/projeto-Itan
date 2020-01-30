9cfd0f5978587226d17ec3a561788048e9732bba
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-01-30 16:56:34 -0300

    Adiciona referências

---
+++ src/components/Referencia/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './style.css'
+
+function Referencia(props) {
+    return (
+       
+            <div className='caixa-container'>
+           
+                    <h3 className='caixa-descricao'> REFERÊNCIAS:</h3>
+
+                    <ul>
+                        <li className='caixa-descrição'>BETHELL, Leslie. A Abolição do comércio brasileiro de escravos: a Grã-Bretanha, o Brasil e a questão do comércio de escravos 1807-1869. Tradução: Luís A.P. Souto Maior. Brasília: Senado Federal, Conselho Editorial, 2002. 478p</li>
+                        <li className='caixa-descrição'>BRITO, Luciana da Cruz. Temores da África: segurança, legislação e população africana na Bahia oitocentista. Salvador: EDUFBA, 2016, 229 p.</li>
+                        <li className='caixa-descrição'>CASTILLO, Lisa Earl. PARÉS, Luis Nicolau. Marcelina da Silva e seu mundo: novos dados para a historiografia do candomblé Ketu. Afro-Ásia, 36 (2007), 111-151.</li>
+                        <li className='caixa-descrição'>CASTILLO, Lisa Earl. Entre memória, mito e história: viajantes transatlãnticos da Casa Branca, in João José Reis e Elciene Azevedo (org.), Escravidão e suas sombras, Salvador, Edufba, 2012, pp. 65- 110.</li>
+                        <li className='caixa-descrição'>CHALHOUB, Sidney. 2012. A força da escravidão. Ilegalidade e costume no Brasil oitocentista. São Paulo: Companhia das Letras, p. 34</li>
+                        <li className='caixa-descrição'>CUNHA, Manuela Carneiro da. Negros, estrangeiros: os escravos libertos e sua volta à África — 2a ed. rev. ampl. — São Paulo: Companhia das Letras, 2012</li>
+                        <li className='caixa-descrição'>LAW, Robin. A comunidade brasileira de Uidá e os últimos anos do tráfico atlântico de escravos, 1850-66. Afro-Ásia, 27 (2002), 41-77</li>
+                        <li className='caixa-descrição'>MATA, Iacy Maia. Conspirações da raça de cor: escravidão, liberdade e tensões raciais em Santiago de Cuba (1864-1881). Campinas, SP: Editora da Unicamp, 2015.</li>
+                        <li className='caixa-descrição'>PARÉS, L. Nicolau. Milicianos, barbeiros e traficantes numa irmandade católica de africanos minas e jejes (Bahia, 1770-180). Revista Tempo/2014, v.20, p.1-32BRITO, Luciana da Cruz. Temores da África: segurança, legislação e população africana na Bahia oitocentista. Salvador: EDUFBA, 2016, 229 p.</li>
+                        <li className='caixa-descrição'>REIS, João José. Entre parentes: nações africanas na cidade da Bahia, século XIX. IN: SOUZA, Everton S. MARQUES, Guida. SILVA, Hugo R.(org.) Salvador da Bahia: retratos de uma cidade atlântica – Salvador, Lisboa: EDUFBA, CHAM, 2016. p. 273/309.</li>
+                        <li className='caixa-descrição'>SOARES, Cecília Moreira. As Ganhadeiras: mulher e resistência negra em Salvador no século XIX. Afro-Ásia n°17 (1996), pp. 57-71.</li>
+                        <li className='caixa-descrição'>VERGER, Pierre. Fluxo e refluxo do tráfico de escravos entre o Golfo do Benin e a Bahia de Todos os Santos: dos séculos XVII a XIX; 4. ed. rev. - Salvador: Corrupio, 2002. 732p.</li>
+                        
+                    </ul>
+                    
+
+            </div>    
+    )
+}
+
+export default Referencia
