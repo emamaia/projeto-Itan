78e16eeaa483f9ba7f0d3a5ac879f4302eebff29
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-03-26 19:37:07 -0300

    conserta random

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -10,32 +10,37 @@ import principal from '../../assets/principal.png'
 import './style.css'
 import CaixaTexto from '../../components/CaixaTexto';
 
-class Home  extends React.Component{
-    constructor(props){
+class Home extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
             dica: [],
             dicaAtual: [],
-            msgErro:[]
+            msgErro: []
         }
     }
 
     componentDidMount() {
         getPistas()
-            .then(response => {               
-                
+            .then(response => {
+                console.log(response.data)
                 this.setState({
-                  dica: response.data,
-                  dicaAtual: response.data[0],
-                  index: 0
-                })           
+                    dica: response.data,
+                    dicaAtual: response.data[0],
+                    index: 0
+                })
 
             })
             .catch(error => {
                 console.error(error)
-            })           
+            })
+                           
     }
-    
+
+    randOrd = () => {
+        return (Math.round(Math.random())-0.5);        
+    }
+
 
     handleMudaPista = () => {
         let mudaIndex = this.state.index + 1
@@ -43,80 +48,77 @@ class Home  extends React.Component{
             dicaAtual: this.state.dica[mudaIndex],
             index: mudaIndex
         })
-      
+
     }
 
     handleErro = () => {
-              this.setState({
-                msgErro: this.state.dica[5]            
-             })
-             this.props.history.push({
-                 pathname: '/erro'
-             })
-             
-    }      
+        this.setState({
+            msgErro: this.state.dica[5]
+        })
+        this.props.history.push({
+            pathname: '/erro'
+        })
+
+    }
+
+    handleResposta = (value) => {
+        if (value) {
+            this.handleMudaPista()
+        } else {
+            this.handleErro()
+        }
+    }
 
 
-    render(){       
-                       
-        return(       
-        <div>
-            <Header 
-            titulo = 'A Jornada de Itan'
-            imagem={principal}           
-            historia ='Itan tinha poucos anos de vida quando foi levado à força para um navio negreiro, junto à sua mãe, Inaê. Nascidos em um povoado na costa africana, mãe e filho tiveram suas vidas submetidas à escravidão em meados de 1840. 
+    render() {
+
+        return (
+            <div>
+                <Header
+                    titulo='A Jornada de Itan'
+                    imagem={principal}
+                    historia='Itan tinha poucos anos de vida quando foi levado à força para um navio negreiro, junto à sua mãe, Inaê. Nascidos em um povoado na costa africana, mãe e filho tiveram suas vidas submetidas à escravidão em meados de 1840. 
             Ao chegar na costa brasileira, Inaê e Itan foram separados, vendidos como mercadoria a pessoas diferentes. Anos se passaram até que Inaê conseguiu  comprar sua liberdade, à custa de vender quitutes como ganhadeira. Já liberta, Inaê foi em busca do seu filho perdido. Ela sabia que a jornada seria extramamente difícil, mas com a rede de apoio formada por africanos libertos e escravizados, Inaê partiu em busca de Itan.'
-            instrucao='Você pode fazer parte desta rede e ajudar Inaê? Basta seguir as pistas para acharmos ITAN:'                     
-            />        
-            
-            {this.state.dicaAtual == this.state.dica[4] ? 
-            
-            <div className='game-pista'>
-                <h3>{this.state.dicaAtual.titulo}</h3>
-                <p>{this.state.dicaAtual.descricao}</p>
-            </div> : 
-            <div className='game-container'>
-                 <div className='game-pista'>                      
-                     <h3>{this.state.dicaAtual.titulo}</h3>
-                     <p>{this.state.dicaAtual.descricao}</p>
-                     
-                 </div>
+                    instrucao='Você pode fazer parte desta rede e ajudar Inaê? Basta seguir as pistas para acharmos ITAN:'
+                />
 
-            <div className='game-options'>
-                <Button
-                handleClick={this.handleErro}
-                >
-                    {this.state.dicaAtual.opcao1}
-                </Button>
+                {this.state.dicaAtual.options === undefined ?
 
-                <Button
-                handleClick={this.handleMudaPista}
-                >
-                    {this.state.dicaAtual.opcao2}
-                </Button>
+                    <div className='game-pista'>
+                        <h3>{this.state.dicaAtual.titulo}</h3>
+                        <p>{this.state.dicaAtual.descricao}</p>
+                    </div> :
+                    <div className='game-container'>
+                        <div className='game-pista'>
+                            <h3>{this.state.dicaAtual.titulo}</h3>
+                            <p>{this.state.dicaAtual.descricao}</p>
 
-                <Button
-                handleClick={this.handleErro}
-                >
-                    {this.state.dicaAtual.opcao3}
-                </Button>
+                        </div>
 
-                <Button
-                handleClick={this.handleErro}
-                >
-                    {this.state.dicaAtual.opcao4}
-                </Button>
+                        <div className='game-options'>
+
+                            {this.state.dicaAtual.options.sort(this.randOrd).map(item => {
+
+                                return (
+                                    <Button
+                                        handleClick={() => this.handleResposta(item.value)}
+                                    >
+                                        {item.label}
+                                    </Button>
+
+                                )
+                            })
+                            }
+                        </div>
+
+
+                    </div>
+                }
+
 
             </div>
-                
-            
-        </div>
+        )
     }
-          
-
-        </div>
-     )  
-}
 }
 
 export default Home
--- src/service/base.js
+++ src/service/base.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const config = {
   baseURL: 'https://my-json-server.typicode.com/emamaia/pistas',
-  timeout: 1000
+  timeout: 10000
 }
 
 const protocolo = axios.create(config)
